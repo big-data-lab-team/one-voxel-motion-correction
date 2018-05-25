2fac539c6c346b306b04710226845b470ea25fcf
Tristan Glatard <tristan.glatard@concordia.ca>
2018-05-25 15:13:48 -0400

    Added test for mcflirt_ovmc

---
+++ test/test_mcflirt.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from boutiques import bosh
+from unittest import TestCase
+import os
+
+
+class TestMcFlirt(TestCase):
+
+    def run_command_test(self, command):
+        process = subprocess.Popen(command, shell=True,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+        self.assertFalse(process.returncode)
+        return process.stdout.read().decode("utf-8")
+
+
+    def test_mcflirt(self):
+        bosh.test("boutiques/mcflirt_ovmc.json")
